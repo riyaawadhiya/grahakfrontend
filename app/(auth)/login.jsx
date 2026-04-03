71ed4966cb1604a2fcf8b35b0ed6085e3659f739
riyaawadhiya <riaawadhiya8200@gmail.com>
2026-04-03 23:47:00 +0530

    Template

---
+++ app/(auth)/login.jsx
@@ -0,0 +1,13 @@
+import { View, Text, Button } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function Login() {
+  const router = useRouter();
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Login Screen</Text>
+      <Button title="Login" onPress={() => router.replace("/home")} />
+    </View>
+  );
+}
